37f5eb234f8e9dc79c264a2e4729a39ec7105c62
rgallego87 <rgdmax@gmail.com>
2018-11-26 15:25:57 +0100

    V1 with local json

--- src/components/chart3.vue
+++ src/components/chart3.vue
@@ -10,8 +10,10 @@
 // var Plotly = require('plotly.js/lib/core');
 // Plotly.register(require('plotly.js/lib/surface'));
 
-const x_times = [];
+const nSpectres = 20;
+const precision = 10;
 let x_fake = 1;
+const x_times = [];
 const y_freqs = [];
 const z_vibras = [];
 
@@ -25,24 +27,25 @@ export default {
     methods: {
         async fetchData() {
             try {
-                const data = await fetch('http://predictivepumpsapi.azurewebsites.net/api/Spectrum/GetSpectrumByDates/RPG/GA-859-S/1V/2018-10-11%2000%3A08%3A18.907/2018-10-11%2003%3A43%3A16.433');
-                const json = await data.json();
+                // const data = await fetch('http://predictivepumpsapi.azurewebsites.net/api/Spectrum/GetSpectrumByDates/RPG/GA-859-S/1V/2018-10-11%2000%3A08%3A18.907/2018-10-11%2003%3A43%3A16.433');
+                // const json = await data.json();     
+                const json = await require('../../public/chart-test.json');
 
                 // console.log(json);
 
                 // this.x_time = Date.parse(json.SourceTimeStamp);
 
                 // Quantity of plots to draw with i (performance)
-                for (let i = 0; i <= 2; i++) {
+                for (let i = 0; i < nSpectres; i++) {
+                    x_fake += 2;
                     json[i].Spectrum.forEach((element, index) => {
                         // Filtering only multiples of 10 to reduce results
-                        if (index % 1000 === 0) {
+                        if (index % precision === 0) {
                             // Inserting two times same
                             // x_times.push(new Array(index, index + 1));                            
                             x_times.push(new Array(x_fake, x_fake + 1));
                             y_freqs.push(new Array(element.frecuencia, element.frecuencia));
                             z_vibras.push(new Array(element.vibracion, element.vibracion));
-                            x_fake++;
                         }
                     });
                 }
@@ -62,6 +65,7 @@ export default {
                 x: x,
                 y: y,
                 z: z,
+                // name: `spectre${x_fake}`,
                 type: 'surface',
                 autocolorscale: true,
                 showscale: false
@@ -105,7 +109,8 @@ export default {
             // }
 
             let data = [];
-            for (let i = 0; i < 3; i++) {
+            let nTraces = nSpectres;
+            for (let i = 0; i < nTraces; i++) {
                 data.push(this.generateTrace(x_times, y_freqs, z_vibras));
             }
 
